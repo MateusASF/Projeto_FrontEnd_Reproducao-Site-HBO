7c9865080125c46860466c52dd1b37b47cfd29cb
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2022-10-27 18:55:24 -0300

    correção de menu nas páginas individuais

---
+++ PROJETO FINAL/pages/Sergio/sergio.js
@@ -0,0 +1,7 @@
+function clickMenu(){
+    menuLateral.style.display = menuLateral.style.display == 'block' ? 'none' : 'block';
+}
+
+function closeMenu(){
+    menuLateral.style.display = 'none';
+}
