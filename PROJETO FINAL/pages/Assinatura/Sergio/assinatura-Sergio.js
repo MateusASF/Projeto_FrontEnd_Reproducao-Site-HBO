32e8424f21ff33bc25ec1e45fe72c135b97d5b74
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-10-28 11:25:40 -0300

    removido console log js

--- PROJETO FINAL/pages/Assinatura/Sergio/assinatura-Sergio.js
+++ PROJETO FINAL/pages/Assinatura/Sergio/assinatura-Sergio.js
@@ -2,7 +2,6 @@ let form = document.querySelector("form");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let data = new FormData(form);
-    console.log(data);
     fetch('https://script.google.com/macros/s/AKfycbxewNWf-q5KJAxBqpld2DPpXhSvJjOf3fwrOf1gqW6nW4b5nPhSp6U4GnBOexvux9IM/exec', {
         method: "POST",
         body: data
